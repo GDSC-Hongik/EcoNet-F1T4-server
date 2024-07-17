91e65dd6de48ab8d09aa1f9cb2b36fd9187434ab
정원희 <jeong-wonhee@jeong-wonhuiui-noteubug.local>
2024-07-17 15:53:00 +0900

    메인페이지에서 이동할 url추가

--- econet/econet/urls.py
+++ econet/econet/urls.py
@@ -20,5 +20,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 #    path('', include()),  //메인페이지로 연결
     path('accounts/', include('accounts.urls')),
-    
+    path('board/', include('board.urls')),
+    path('today/', include('today.urls')),
+    path('article/', include('article.urls')),
+    path('location/', include('location.urls')),
+
 ]
