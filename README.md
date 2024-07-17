994d6695b384f6de6ce6c92e426c799d07c6e464
goldenGlow21 <jonghoon8928@gmail.com>
2024-07-17 16:39:32 +0900

    add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# EcoNet by F1T4
+## Server Part Repository
+
+# Contributor
+
+- goldenGlow21
+- oneeee822
+
+# Specification
+
+- Python 3.7.7
+- Django 3.2.25
--- econet/accounts/urls.py
+++ econet/accounts/urls.py
@@ -7,6 +7,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup),
+#    path('', )  마이페이지(프로필창)
     path('login/', auth_views.LoginView.as_view(template_name = "accounts/login.html"), name = "login"),
     path('logout/', auth_views.LogoutView.as_view(next_page=""), name = "logout"),
 ]
