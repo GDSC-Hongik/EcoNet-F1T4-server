188ce59b1fcb5df952986a682a09ea61ea6e2467
oneeee822 <bestoneeee@naver.com>
2024-07-19 15:22:47 +0900

    board앱 안에 있는  게시판목록과 게시글목록에 필요한  view,form,url 작성

--- econet/board/models.py
+++ econet/board/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    board_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Board'
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
     title = models.IntegerField()
     content = models.CharField(max_length=300)
     created_dt = models.DateTimeField(blank=True, null=True)
     likes = models.IntegerField(blank=True, null=True)
-    board_id = models.IntegerField(blank=True, null=True)
+    board = models.ForeignKey(Board, models.DO_NOTHING, blank=True, null=True)
     user_id = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'Post'
+    
+    def __str__(self):
+        return self.title
 
 class Scrap(models.Model):
     scrap_id = models.AutoField(primary_key=True)
--- econet/board/views.py
+++ econet/board/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Board, Post
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("")
+def board_list(request):
+    boards = Board.objects.all()  # 모든 게시판을 조회
+    return render(request, 'board_list.html', {'boards': boards})
+
+def post_list(request, board_id):
+    #id에 맞는 게시판 
+    board = get_object_or_404(Board, id=board_id)
+    # 필터 조건에서 'board' 사용
+    posts = Post.objects.filter(board=board)
+    return render(request, 'post_list.html', {'board': board, 'posts': posts})
