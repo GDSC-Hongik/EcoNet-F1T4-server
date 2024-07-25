16f325409654849cf7a8c36685bcf186e657a014
oneeee822 <bestoneeee@naver.com>
2024-07-25 16:55:03 +0900

    gitignore 파일 수정

--- econet/econet/settings.py
+++ econet/econet/settings.py
@@ -10,19 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, json
 import pymysql  
 pymysql.install_as_MySQLdb()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1gv_c)2zvcq1=wk5gv9j03joy9b-xkt55cwus!^8f)ie)oaayt'
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
