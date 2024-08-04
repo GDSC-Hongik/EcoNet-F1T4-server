f791a6e52853c333d9f480671f98e8f4a53da19f
oneeee822 <bestoneeee@naver.com>
2024-08-04 23:05:31 +0900

    feat: add info_comment feature to bin detail page

--- econet/maps/urls.py
+++ econet/maps/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import bin_list, bin_detail
+from .views import bin_list, bin_detail, create_picture, create_info
 
 urlpatterns = [
     path('', bin_list, name='bin_list'),
     path('<int:pk>/', bin_detail, name='bin_detail'),
+    path('<int:pk>/pic_upload/', create_picture, name='create_picture'),
+    path('<int:pk>/info_upload/', create_info, name='create_info')
 ]
