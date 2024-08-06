53ffbb0dc80c58aba62b8f3dc055e8cb61ee0ead
oneE <bestoneeee@naver.com>
2024-08-06 22:03:17 +0900

    feat: Add functionality to retrieve Seoul district information (#36)

--- econet/todays/urls.py
+++ econet/todays/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DistrictListView
+from .views import DistrictListView, naver_map
 
 urlpatterns = [
     path("", DistrictListView.as_view(), name='district-list'),
+    path("map/", naver_map, name='naver_map'),
 ]
