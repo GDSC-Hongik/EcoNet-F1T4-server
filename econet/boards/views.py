2bd99286962b7edd7542ccdf12ffb32a758eec5d
goldenGlow21 <122194281+goldenGlow21@users.noreply.github.com>
2024-08-06 23:14:05 +0900

    Fix: HTTP 메소드에 상관없이 사용자 인증 요청하던 문제 해결 (#38)
    * Fix: 메소드 상관 없이 인증 요구하던 문제 해결

--- econet/boards/views.py
+++ econet/boards/views.py
@@ -7,8 +7,9 @@ from .models import Gathering, Comment
 from .serializers import GatheringSerializer, GatheringDetailSerializer, CommentCreateSerializer, GatheringCreateSerializer, GatheringUpdateSerializer
 
 @api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
 def gathering_list_create(request):
-    if request.method == 'GET':
+    if request.method == 'GET': # 모임 목록 조회
         page = request.query_params.get('page', 1)
         limit = request.query_params.get('limit', 10)
 
@@ -23,7 +24,7 @@ def gathering_list_create(request):
         serializer = GatheringSerializer(gatherings, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
+    elif request.method == 'POST':  # 모임 생성
         if not request.user.is_authenticated:
             return Response({"error": "먼저 사용자 인증이 필요합니다!"}, status=status.HTTP_401_UNAUTHORIZED)
 
@@ -34,29 +35,30 @@ def gathering_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PATCH', 'DELETE'])
+@permission_classes([AllowAny])
 def gathering_detail_update_delete(request, gatheringpost_id):
     try:
         gathering = Gathering.objects.get(id=gatheringpost_id)
     except Gathering.DoesNotExist:
         return Response({"error": "모임이 존재하지 않습니다"}, status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    if request.method == 'GET': # 모임 상세정보 조회
         serializer = GatheringDetailSerializer(gathering)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'PATCH':
+    elif request.method == 'PATCH': # 모임 정보 수정
         serializer = GatheringUpdateSerializer(gathering, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    elif request.method == 'DELETE':    # 모임 삭제
         gathering.delete()
         return Response({"message": "모임이 삭제되었습니다."}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
-def create_comment(request, gatheringpost_id):
+def create_comment(request, gatheringpost_id):  # 댓글 작성
     try:
         gathering = Gathering.objects.get(id=gatheringpost_id)
     except Gathering.DoesNotExist:
--- econet/econet/settings.py
+++ econet/econet/settings.py
@@ -66,14 +66,16 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',  # 인증된 요청인지 확인
+    #     'rest_framework.permissions.IsAdminUser',  # 관리자만 접근 가능
+    #     'rest_framework.permissions.AllowAny',  # 누구나 접근 가능
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',  # JWT를 통한 인증방식 사용
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
 
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',  # 모든 요청에 대해 인증 요청
         'rest_framework.permissions.AllowAny',
     ),
 }
